dc7f2904c9ea3f14472ea98831ee7480eb9c6d91
leticiabedoni <leticiabedoni@hotmail.com>
2025-07-05 21:19:03 -0300

    define nome do repositorio nas variavei

--- automation/config.py
+++ automation/config.py
@@ -5,6 +5,6 @@ load_dotenv()  # carrega as variáveis do .env para o ambiente
 
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 REPO_OWNER = 'leticiabedoni'
-REPO_NAME = 'nome-do-repo'
+REPO_NAME = 'github-issues-report'
 USUARIOS_DE_INTERESSE = ['leticiabedoni']
 
